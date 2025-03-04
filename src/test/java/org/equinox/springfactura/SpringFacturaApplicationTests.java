e4eef6f797e5199748a6822f2c5f40a7ecaee1d8
equinox766 <Equinox766@gmail.com>
2025-03-04 14:37:52 -0300

    first commit

---
+++ src/test/java/org/equinox/springfactura/SpringFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.equinox.springfactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
